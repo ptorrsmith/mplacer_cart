e813761124504bc59ed1f8e5a179f068c0e7977f
Peter <peter@ptorrsmith.com>
2022-02-22 00:27:40 +1300

    spec contexts, test multiple line_items totals

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -5,26 +5,44 @@ RSpec.describe 'Cart' do
   let (:cart) { Cart.new }
   let (:line_item) { LineItem.new(product_id = 123, quantity=1, unit_price="100", name="Widget")}
 
-  it 'has empty zero values' do
-    subtotal = cart.subtotal
-    discount = cart.discount
-    total = cart.total
+  context 'The cart is empty' do
+    it 'has empty zero values' do
+      subtotal = cart.subtotal
+      discount = cart.discount
+      total = cart.total
 
-    expect(subtotal).to eq(0)
-    expect(discount).to eq(0)
-    expect(total).to eq(0)
+      expect(subtotal).to eq(0)
+      expect(discount).to eq(0)
+      expect(total).to eq(0)
+    end
   end
 
-  it 'returns the correct total, discount and subtotal' do
-    cart.line_items << line_item
+  context 'an item is added to the cart' do
+    it 'returns the correct total, discount and subtotal' do
+      cart.line_items << line_item
 
-    subtotal = cart.subtotal
-    discount = cart.discount
-    total = cart.total
+      subtotal = cart.subtotal
+      discount = cart.discount
+      total = cart.total
 
-    expect(subtotal).to eq(100)
-    expect(discount).to eq(15)
-    expect(total).to eq(85)
+      expect(subtotal).to eq(100)
+      expect(discount).to eq(15)
+      expect(total).to eq(85)
+    end
   end
 
+  context 'another identical item is added to the cart' do
+    it 'returns the correct total, discount and subtotal' do
+      cart.line_items << line_item
+      cart.line_items << line_item
+
+      subtotal = cart.subtotal
+      discount = cart.discount
+      total = cart.total
+
+      expect(subtotal).to eq(200)
+      expect(discount).to eq(40)
+      expect(total).to eq(160)
+    end
+  end
 end
