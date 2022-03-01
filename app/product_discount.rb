5629a4f5fb762a144ae8d9db0ddc753ebe6c1c8c
Peter <peter@ptorrsmith.com>
2022-03-01 17:25:12 +1300

    introduce ProductDiscount class

---
+++ app/product_discount.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+class ProductDiscount
+	attr_accessor :product_uuid, :qty_threshold, :unit_price
+
+	def initialize(json)
+		@product_uuid =  json["product_uuid"]
+		@unit_price =  json["unit_price"].to_f
+		@qty_threshold =  json["qty_threshold"].to_i
+	end
+end
